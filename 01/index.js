20ee6f48e1b12f9705d9bfb381c70470c64e875e
Fábio Cavalcanti <fabiocavalcanti.mkt@gmail.com>
2023-01-24 14:16:12 -0300

    Exercício 01 Cadastro de usuário Finalizado

---
+++ 01/index.js
@@ -0,0 +1,20 @@
+const endereco = {
+    rua: "Rua Antonio Coelho",
+    numero: 115,
+    complemento: "Por trás do Atacadão",
+    bairro: "Varzea",
+    cep: 50740030,
+    cidade: "Recife",
+    estado: "Pernambuco",
+    pais: "Brasil"
+};
+
+const usuario = {
+    nome: "Fábio Cavalcanti",
+    email: "fabiocavalcanti.mkt@gmail.com",
+    telefone: 81999556312,
+    data_nascimento: "21-10-1996",
+    endereco: endereco
+};
+
+console.log(usuario);
