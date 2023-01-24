fa3ea573bfcc0e9a0973402e33cecf978b7cd5f1
Fábio Cavalcanti <fabiocavalcanti.mkt@gmail.com>
2023-01-24 14:21:29 -0300

    Exercício 02 Lista de veículos Finalizado

---
+++ 02/index.js
@@ -0,0 +1,12 @@
+const carro = {
+    marca: "Chevrolet",
+    modelo: "Onix",
+    ano: 2020,
+    cor: "preto",
+    quantidade_portas: 4,
+    airbag: true,
+    ar_condicionado: true,
+    automatico: true
+};
+
+console.log(carro);
